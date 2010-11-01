3885d091cde92a0ee132b01e31d0b8dea273c3d6
Zac Clark <spyyddir@yahoo.com>
2010-11-01 11:20:02 -0600

    Mocked out dispatcher and removed unneeded files.

--- src/main/java/AuthServlet.java
+++ src/main/java/AuthServlet.java
@@ -6,16 +6,12 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 
-public class DispatchServlet extends HttpServlet {
-    public static final String VIEW = "/WEB-INF/jsp/dispatcher.jsp";
+public class AuthServlet extends HttpServlet {
+    public static final String VIEW = "/WEB-INF/jsp/auth.jsp";
 
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		if (true) {									// do proper check
-			resp.sendRedirect("login");
-		}else{
-			resp.sendRedirect("app"); 				// change this
-		}
+      	req.getRequestDispatcher(VIEW).forward(req, resp);
     }
 }
