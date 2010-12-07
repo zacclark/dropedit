3a4d9a432f3f554f994229ed4e7173659b13b54d
Zac Clark <spyyddir@yahoo.com>
2010-12-07 16:45:03 -0700

    Added test to make sure ListServlet executes fully, with general Dropbox login. Also removed system out print from ListServlet.

--- src/test/java/com/dropedit/controller/ListServletTest.java
+++ src/test/java/com/dropedit/controller/ListServletTest.java
@@ -8,8 +8,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.servlet.RequestDispatcher;
 import java.io.IOException;
 import com.dropedit.controller.ListServlet;
+import com.dropbox.client.*;
+import java.util.Map;
 
 @Test
 public class ListServletTest {
@@ -23,6 +26,29 @@ public class ListServletTest {
 		new ListServlet().doGet(req, resp);
 		
 		verify(resp).sendRedirect("/");
+	}
+	
+	public void requestWithSessionIsForwardedView() throws Exception {
+		HttpServletRequest req = mock(HttpServletRequest.class);
+		HttpServletResponse resp = mock(HttpServletResponse.class);
+		HttpSession sesh = mock(HttpSession.class); // mock the session
+		
+		when(req.getSession(false)).thenReturn(sesh); // use mock session
+		when(sesh.getAttribute("uname")).thenReturn("test");
+		
+		// create a new dropbox client, as if it was created by the AuthServlet
+		Map config = Authenticator.loadConfig("config/keys.json");
+		Authenticator auth = new Authenticator(config);
+		DropboxClient dropbox = new DropboxClient(config, auth);
+		
+		when(sesh.getAttribute("client")).thenReturn(dropbox);
+		
+		RequestDispatcher dispatcher = mock(RequestDispatcher.class);
+		when(req.getRequestDispatcher("/WEB-INF/jsp/list.jsp")).thenReturn(dispatcher);
+		
+		new ListServlet().doGet(req, resp);
+		
+		verify(dispatcher).forward(req, resp);
 	}
 	
 }
